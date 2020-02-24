d60106bf6e8df99e79f0331d87ebc91ed3aa423c
Rafał Maciejewski <rafaljestsuper@gmail.com>
2020-02-24 09:42:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+Using Node v12.16.1 on 2,2 GHz Quad-Core Intel Core i7
+
+5 runs for each case
+
+## bind.js
+
+```
+Time: 6ms
+Memory: 4669
+
+Time: 5ms
+Memory: 4664
+
+Time: 6ms
+Memory: 4637
+
+Time: 6ms
+Memory: 4624
+
+Time: 6ms
+Memory: 4665
+```
+
+## class-fields.js
+
+```
+Time: 12ms
+Memory: 4957
+
+Time: 12ms
+Memory: 4915
+
+Time: 12ms
+Memory: 4900
+
+Time: 12ms
+Memory: 4905
+
+Time: 12ms
+Memory: 4922
+```
---
+++ bind.js
@@ -0,0 +1,34 @@
+const { performance } = require('perf_hooks');
+
+class Test {
+    constructor() {
+        this.a = this.a.bind(this);
+        this.b = this.b.bind(this);
+        this.c = this.c.bind(this);
+    }
+
+    a() {
+        return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat, sapien nec feugiat vestibulum, sapien orci egestas eros, malesuada feugiat libero lectus bibendum arcu. Suspendisse finibus velit odio, a pretium sapien malesuada vitae. Mauris scelerisque ut massa nec imperdiet. Cras consequat risus diam, a mollis metus vehicula sit amet. Suspendisse aliquam, augue id condimentum rutrum, ipsum ipsum congue nibh, sit amet mollis nulla erat faucibus arcu. Quisque venenatis ligula non odio aliquam, a aliquet ligula rutrum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris non semper felis. Donec semper vehicula pharetra. Fusce tempor sodales orci, ac dapibus risus feugiat et. Sed non sem et magna ullamcorper facilisis et sed lacus. Suspendisse at tempor lacus. Sed in leo a turpis pharetra dictum. Mauris orci tellus, sagittis in viverra ac, placerat vitae dui. Cras erat leo, fermentum vitae molestie volutpat, pretium eget ex. Nam faucibus risus ac arcu eleifend, sed pellentesque massa semper.';
+    }
+
+    b() {
+        return 'Nullam eros odio, porttitor eget quam a, fringilla fermentum mi. Cras volutpat turpis eu mi molestie, vitae vulputate est commodo. Vestibulum sed massa a eros mollis rhoncus vel vel arcu. Maecenas facilisis, mi id egestas rutrum, ante libero cursus risus, a ultrices odio nunc a ante. Mauris laoreet nibh non enim vestibulum facilisis. Donec mauris neque, rhoncus ut interdum ac, sagittis porta orci. Nam pellentesque sodales urna ut feugiat. Nam nec consequat odio. Etiam tempor metus a euismod luctus. Sed gravida, lacus in euismod commodo, ex lorem feugiat dui, vitae porta odio est eu elit. Vestibulum porttitor quam sed laoreet tristique. Donec nec gravida nibh. Sed sit amet eleifend sem. Maecenas sollicitudin urna eget ex suscipit, ultrices finibus mi varius. Mauris ultricies laoreet nibh, sit amet tincidunt quam facilisis in.';
+    }
+
+    c() {
+        return 'Ut varius rutrum lacinia. Etiam accumsan velit ac iaculis malesuada. Proin quis fringilla lorem. Sed fringilla nisi enim, et tristique turpis pellentesque sed. Praesent volutpat ullamcorper massa vel eleifend. Ut ultrices quis enim quis fermentum. Vivamus facilisis, lacus in rhoncus accumsan, augue lectus rutrum ipsum, id dictum quam magna eu metus.';
+    }
+}
+
+const result = [];
+
+const t0 = performance.now();
+for (let i = 0; i < 10000; i++) {
+    result.push(new Test());
+}
+const t1 = performance.now();
+
+console.log(`Time: ${(t1 - t0).toFixed()}ms`);
+console.log(`Memory: ${(process.memoryUsage().heapUsed / 1024).toFixed()}`);
+
+module.exports = result;
